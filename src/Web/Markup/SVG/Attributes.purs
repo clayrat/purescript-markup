1ea273b336fa6c6388a5c54c4cbd6fd2e811758d
Alex Gryzlov <alexander.gryzlov@adform.com>
2017-02-13 20:23:20 +0200

    add attributes

--- src/Web/Markup/SVG/Attributes.purs
+++ src/Web/Markup/SVG/Attributes.purs
@@ -11,11 +11,20 @@ cy = Attr "cy"
 d :: forall e. String -> Prop e
 d = Attr "d"
 
+dx :: forall e. String -> Prop e
+dx = Attr "dx"
+
 dy :: forall e. String -> Prop e
 dy = Attr "dy"
 
 fill :: forall e. String -> Prop e
 fill = Attr "fill"
+
+fontFamily :: forall e. String -> Prop e
+fontFamily = Attr "font-family"
+
+fontSize :: forall e. String -> Prop e
+fontSize = Attr "font-size"
 
 height :: forall e. String -> Prop e
 height = Attr "height"
@@ -23,8 +32,14 @@ height = Attr "height"
 r :: forall e. String -> Prop e
 r = Attr "r"
 
+rotate :: forall e. String -> Prop e
+rotate = Attr "rotate"
+
 strokeWidth :: forall e. String -> Prop e
 strokeWidth = Attr "stroke-width"
+
+textAnchor :: forall e. String -> Prop e
+textAnchor = Attr "text-anchor"
 
 transform :: forall e. String -> Prop e
 transform = Attr "transform"
@@ -32,11 +47,17 @@ transform = Attr "transform"
 width :: forall e. String -> Prop e
 width = Attr "width"
 
+x :: forall e. String -> Prop e
+x = Attr "x"
+
 x1 :: forall e. String -> Prop e
 x1 = Attr "x1"
 
 x2 :: forall e. String -> Prop e
 x2 = Attr "x2"
+
+y :: forall e. String -> Prop e
+y = Attr "y"
 
 y1 :: forall e. String -> Prop e
 y1 = Attr "y1"
